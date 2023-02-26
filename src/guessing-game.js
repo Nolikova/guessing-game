ed9039f1900b835fc8e2dc65e259b428e2a951fc
Anzhelika Novikova <anzhelika.novikova93@gmail.com>
2023-02-26 19:21:29 +0400

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
-
+    min = null;
+    max = null;
+    current = null;
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
+        if ([this.min, this.max, this.current].some === null) {
+            throw "call setRange method first";
+        }
 
+        this.current = this.min + Math.ceil((this.max - this.min) / 2);
+        return this.current;
     }
 
     lower() {
-
+        this.max = this.current;
     }
 
     greater() {
-
+        this.min = this.current;
     }
 }
 
